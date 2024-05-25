2f1cd706418c6609f751d697b962389906a1fdcc
Stephanie7l <100887395+Stephanie7l@users.noreply.github.com>
2024-05-25 18:48:18 -0300

    Criação do carrossel de imagens

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,32 @@
 import { useNavigate } from "react-router-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {CarouselStyled} from "./Styles";
 
-function Home() {
+function Home() 
+{
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
-
-    return (
-
-     <div>
-        PAGINA HOME
-      </div>
-
- 
-    )
-
+  return (
+    <div>
+      HOME
+      <CarouselStyled showThumbs={false} infiniteLoop={true}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+          <img src="./src/Images/CPE-Home.png"/>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+          <img src="../src/Images/CPE-PSTrainee.png"/>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+          <img src="../src/Images/CPE-NossosServiços.png"/>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+          <img src="../src/Images/CPE-NossaHistoria.png"/>
+        </div>
+      </CarouselStyled>
+    </div>
+  );
 }
+
 
 export default Home;
 
---
+++ src/Pages/Home/Styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Carousel } from "react-responsive-carousel";
+
+export const CarouselStyled = styled(Carousel)`
+  width: 400px;
+  height: 120px;
+  margin: auto;
+  margin-top: 50px;
+`
