636ed8b236344196230cc39bfa1c9fae22f99b20
HenriqueMLA <henriqueandrade@cpejr.com.br>
2024-05-27 19:34:29 -0300

    termino da página de projetos

--- src/Pages/Projeto/Projeto.jsx
+++ src/Pages/Projeto/Projeto.jsx
@@ -1,19 +1,27 @@
 import {
   Titulo,
+  SubTitulo,
   Caixa,
-  Faixa,
   Sbutton,
   Input,
   InputMaior,
   Container,
   Descriçao,
+  ProjetoButtons,
+  Linha,
 } from "./Styles";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 function Projeto() {
+  const projetos = [
+    { name: "Ana" },
+    { name: "Stéphanie" },
+    { name: "Henrique" },
+  ];
+
   return (
     <div>
-      <Faixa></Faixa>
-      <Titulo>GERENCIAR PROJETOS</Titulo>
+      <Titulo>CRIAR PROJETOS</Titulo>
 
       <Caixa>
         <Container>
@@ -31,10 +39,21 @@ function Projeto() {
           <Input />
         </Container>
       </Caixa>
-
       <Sbutton onClick={() => alert("Seu projeto foi salvo com sucesso!")}>
         SALVAR
       </Sbutton>
+
+      <SubTitulo>GERENCIAR PROJETOS</SubTitulo>
+
+      {projetos.map((projeto, index) => (
+        <Linha key={index}>
+          <span> {projeto.name} </span>
+          <ProjetoButtons>
+            <FaEdit />
+            <FaTrash />
+          </ProjetoButtons>
+        </Linha>
+      ))}
     </div>
   );
 }
--- src/Pages/Projeto/Styles.js
+++ src/Pages/Projeto/Styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+//import { Table } from "antd";
 
 export const Faixa = styled.div`
   border: solid yellow;
@@ -19,33 +20,35 @@ export const Titulo = styled.div`
   width: 100%; //largura
   height: 100%; //altura
   margin-top: 123px; //distancia do bordo de cima
-  margin-left: 0px; //distancia do bordo da tela
+  margin-left: 15px; //distancia do bordo da tela
   // padding-left: 50%; //distancia de dentro da div
   display: flex;
   justify-content: center; // Centraliza horizontalmente
   //align-items: center; // Centraliza verticalmente
   color: yellow;
 
-  @media (max-width: 0px) {
-    justify-content: space-between;
-    flex-wrap: wrap;
-    padding: 0px;
+  @media (max-width: 405px) {
+    flex-direction: column;
+    justify-content: center;
+    font-size: 30px;
+    // margin-left: 20%;
   }
 `;
 
 export const Caixa = styled.div`
   border: #363636;
   height: 410px;
-  width: 716px;
-  display: flex; //
+  width: 80%;
+  display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  padding-right: 20px;
-  padding-left: 20px;
+  padding-right: 50px;
+  padding-left: 30px;
   margin: auto;
   border-radius: 10px;
   background-color: #363636;
   margin-top: 50px;
+  max-width: 716px;
 
   @media (max-width: 0px) {
     justify-content: space-between;
@@ -54,17 +57,22 @@ export const Caixa = styled.div`
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   font-size: 25px;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+    align-items: start;
+  }
 `;
 
 export const Input = styled.input`
   border-radius: 25px;
   background-color: white;
   text-indent: 10px;
-  width: 477px;
+  width: 75%;
   height: 65px;
   color: black;
   font-size: 26px;
@@ -72,20 +80,29 @@ export const Input = styled.input`
   justify-content: center;
   align-items: center;
   cursor: pointer;
+
+  @media (max-width: 700px) {
+    width: 100%;
+    margin: 0px;
+  }
 `;
 
 export const Descriçao = styled.div`
   display: flex;
-  //justify-content: space-between;
+  justify-content: space-between;
   width: 100%;
   font-size: 26px;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
 `;
 
 export const InputMaior = styled.input`
   border-radius: 25px;
   background-color: white;
   text-indent: 10px;
-  width: 477px;
+  width: 75%;
   height: 140px;
   margin-left: 40px;
   color: black;
@@ -93,6 +110,32 @@ export const InputMaior = styled.input`
   //justify-content: center;
   //align-items: center;
   cursor: pointer;
+
+  @media (max-width: 700px) {
+    width: 100%;
+    margin: 0px;
+  }
+`;
+
+export const SubTitulo = styled.div`
+  //border: solid; //borda
+  width: 100%; //largura
+  height: 100%; //altura
+  margin-top: 80px; //distancia do bordo de cima
+  margin-bottom: 50px;
+  margin-left: 15px; //distancia do bordo da tela
+  // padding-left: 50%; //distancia de dentro da div
+  display: flex;
+  justify-content: center; // Centraliza horizontalmente
+  //align-items: center; // Centraliza verticalmente
+  color: yellow;
+
+  @media (max-width: 405px) {
+    flex-direction: column;
+    justify-content: center;
+    font-size: 30px;
+    // margin-left: 20%;
+  }
 `;
 
 export const Sbutton = styled.button`
@@ -100,7 +143,7 @@ export const Sbutton = styled.button`
   border-radius: 10px;
   background-color: black;
   margin: auto;
-  margin-top: 30px;
+  margin-top: 50px;
   width: 214px;
   height: 50px;
   color: yellow;
@@ -113,4 +156,24 @@ export const Sbutton = styled.button`
   @media (max-width: 0px) {
     justify-content: space-between;
   }
+`;
+
+export const Linha = styled.div`
+  color: white;
+  display: flex;
+  width: 20%;
+  margin-top: 30px;
+  margin-left: 40.6%;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 20%) {
+    flex-direction: column;
+    max-width: 300px;
+    align-items: flex-start;
+  }
+`;
+
+export const ProjetoButtons = styled.div`
+  cursor: pointer;
 `;
